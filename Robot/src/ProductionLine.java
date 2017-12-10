0a3844ac597dac7e997964db4da725e37df347b8
gabbley <baniquedg@bishopireton.org>
2017-12-10 00:18:54 -0500

    whatever

--- Robot/src/ProductionLine.java
+++ Robot/src/ProductionLine.java
@@ -2,32 +2,43 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class ProductionLine {
-	
+
 	private Queue<Disk> input;
 	private Queue<Tower> output;
 	private Tower arm;
 
-	public ProductionLine(){
+	public ProductionLine() {
 		input = new LinkedList<Disk>();
 		output = new LinkedList<Tower>();
-				
+
 	}
-	
-	public void addDisk(Disk d){
+
+	public void addDisk(Disk d) {
 		input.add(d);
 	}
-	
-	public void unloadRobot(){
+
+	public void unloadRobot() {
 		arm.flip();
 		output.add(arm);
+		arm = new Tower();
 	}
-	
-	public void process(){
-		
+
+	public void process() {
+		while (!input.isEmpty()) {
+			Disk d = input.peek();
+			input.remove(d);
+		}
 	}
-	
-	public Disk removeTower(){
-		
+
+	public Tower removeTower() {
+		Tower t = new Tower();
+		if (!output.isEmpty()) {
+			t = output.peek();
+			output.remove(t);
+		} else {
+			t = null;
+		}
+		return t;
 	}
-	
+
 }
